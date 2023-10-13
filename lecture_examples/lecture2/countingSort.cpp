c04a4f3edd0d5884ab818e5abfbf1bc2f685bc14
asen-krasimirov <personalgrowth57@gmail.com>
2023-10-13 21:43:13 +0300

    adding countingSort implementation

---
+++ lecture_examples/lecture2/countingSort.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+int findBiggestNumInVector(vector<int> &arr) {
+    int biggest = -1316134912;
+    for (int curNum : arr) {
+        if (curNum > biggest) biggest = curNum;
+    }
+
+    return biggest;
+}
+*/
+
+vector<int> &countingSort(vector<int> &arr) {
+    vector<int> arrCpy;
+
+    arrCpy.reserve(arr.size()); /* optimization */
+    for (int i: arr) {
+        arrCpy.push_back(i);
+    }
+
+    int *count = new int[256];
+    for (int i: arr) {
+        count[i] = count[i] + 1;
+    }
+
+    for (int i = 1; i < 256; ++i) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = arr.size() - 1; i >= 0; --i) {
+        arr[count[arrCpy[i] - 1]] = arrCpy[i];
+        count[arrCpy[i]] = count[arrCpy[i]] - 1;
+    }
+
+    return arr;
+}
+
+void printArray(vector<int> &arr) {
+    for (auto num: arr) {
+        std::cout << num << " ";
+    }
+}
+
+int main() {
+    vector<int> arr = {34, 44, 27, 12, 37, 10, 47, 30, 20, 41, 15, 33, 14, 29};
+
+    countingSort(arr);
+    printArray(arr);
+
+    return 0;
+}
