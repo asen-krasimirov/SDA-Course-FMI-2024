28b15dbaeecd6a6b8127f9f4fc672badbd09fb13
asen-krasimirov <personalgrowth57@gmail.com>
2023-10-14 00:53:14 +0300

    test commit

--- exercises/two/exercise3.cpp
+++ exercises/two/exercise3.cpp
@@ -7,9 +7,6 @@ using namespace std;
 
 vector<int> sortedSquares(vector<int> &nums) {
     vector<int> sortedSquares;
-    // for (int i = 0; i < nums.size(); ++i) {
-    // sortedSquares.push_back(nums[i] > 0 ? nums[i] : nums[i] * -1);
-    // }
 
     for (int i = 0; i < sortedSquares.size(); ++i) {
         sortedSquares[i] = pow(nums[i], 2);
