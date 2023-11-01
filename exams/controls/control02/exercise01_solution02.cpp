d06cb219c295f175cf039b4a3ec417b26a0f62b9
asen-krasimirov <personalgrowth57@gmail.com>
2023-11-01 23:22:23 +0200

    adding solution to exam control02 task01

---
+++ exams/controls/control02/exercise01_solution02.cpp
@@ -0,0 +1,155 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DoublyLinkedListNode {
+public:
+    int data;
+    DoublyLinkedListNode *next;
+    DoublyLinkedListNode *prev;
+
+    DoublyLinkedListNode(int node_data) {
+        this->data = node_data;
+        this->next = nullptr;
+        this->prev = nullptr;
+    }
+};
+
+class DoublyLinkedList {
+public:
+    DoublyLinkedListNode *head;
+    DoublyLinkedListNode *tail;
+
+    DoublyLinkedList() {
+        this->head = nullptr;
+        this->tail = nullptr;
+    }
+
+    void insert_node(int node_data) {
+        DoublyLinkedListNode* node = new DoublyLinkedListNode(node_data);
+
+        if (!this->head) {
+            this->head = node;
+        } else {
+            this->tail->next = node;
+            node->prev = this->tail;
+        }
+
+        this->tail = node;
+    }
+};
+
+void print_doubly_linked_list(DoublyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_doubly_linked_list(DoublyLinkedListNode* node) {
+    while (node) {
+        DoublyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+
+/*
+ * Complete the 'sortedInsert' function below.
+ *
+ * The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_DOUBLY_LINKED_LIST llist
+ *  2. INTEGER data
+ */
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode* next;
+ *     DoublyLinkedListNode* prev;
+ * };
+ *
+ */
+
+DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* llist, int data) {
+    vector<int> values;
+
+    DoublyLinkedListNode *curr = llist;
+    while(curr != nullptr) {
+        if (curr->data >= data) break;
+        values.push_back(curr->data);
+        curr = curr->next;
+    }
+
+    values.push_back(data);
+
+    while (curr != nullptr) {
+        values.push_back(curr->data);
+        curr = curr->next;
+    }
+
+    if (values.empty()) return new DoublyLinkedListNode(data);
+
+    DoublyLinkedListNode *head;
+    curr = head = new DoublyLinkedListNode(values[0]);
+    DoublyLinkedListNode *prev = nullptr;
+
+    for (int i = 1; i < values.size(); ++i) {
+        curr->prev = prev;
+        prev = curr;
+        curr->next = new DoublyLinkedListNode(values[i]);
+        curr = curr->next;
+    }
+
+    return head;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        DoublyLinkedList* llist = new DoublyLinkedList();
+
+        int llist_count;
+        cin >> llist_count;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        for (int i = 0; i < llist_count; i++) {
+            int llist_item;
+            cin >> llist_item;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            llist->insert_node(llist_item);
+        }
+
+        int data;
+        cin >> data;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        DoublyLinkedListNode* llist1 = sortedInsert(llist->head, data);
+
+        print_doubly_linked_list(llist1, " ", fout);
+        fout << "\n";
+
+        free_doubly_linked_list(llist1);
+    }
+
+    fout.close();
+
+    return 0;
+}
