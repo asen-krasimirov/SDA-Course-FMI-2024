9420d2bb17869ee14d7d53daf51f9ea58b123ed2
asen-krasimirov <personalgrowth57@gmail.com>
2023-11-10 14:35:00 +0200

    adding solution to exercise01 of homework06

--- homeworks/homework06/exercise01.cpp
+++ homeworks/homework06/exercise01.cpp
@@ -5,18 +5,15 @@
 #include <algorithm>
 using namespace std;
 
-/* 70/100 */
-
 struct Node {
-    int val = -1;
-    int breadth;
-    int leftIdx, rightIdx;
+    long long val;
+    long long breadth;
     Node *left, *right;
 } nodes[1000000];
 
-int N, curVal, leftIdx, rightIdx, mostLeftBreadth = 0, mostRightBreadth = 0;
+long long N, curVal, leftIdx, rightIdx, mostLeftBreadth = 0, mostRightBreadth = 0;
 
-void inorderSumUp(Node *current, vector<int> &columnSums) {
+void inorderSumUp(Node *current, vector<long long> &columnSums) {
     if (current == nullptr) {
         return;
     }
@@ -30,10 +27,10 @@ int main() {
     cin >> N >> curVal >> leftIdx >> rightIdx;
 
     nodes[0].val = curVal;
+
     nodes[0].breadth = 0;
 
     if (leftIdx != -1) {
-        nodes[0].leftIdx = leftIdx;
         nodes[0].left = &nodes[leftIdx];
         nodes[0].left->breadth = -1;
     } else {
@@ -41,34 +38,18 @@ int main() {
     }
 
     if (rightIdx != -1) {
-        nodes[0].rightIdx = rightIdx;
         nodes[0].right = &nodes[rightIdx];
         nodes[0].right->breadth = 1;
     } else {
         nodes[0].right = nullptr;
     }
 
-    Node *current = &nodes[0];
-
-    for (int nextCtr = 1, i = 1; nextCtr < N; ++nextCtr) {
-        if (current->leftIdx != nextCtr && current->rightIdx != nextCtr) {
-            if (current->leftIdx != -1) {
-                i = current->leftIdx;
-            } else if (current->rightIdx != -1) {
-                i = current->rightIdx;
-            } else {
-                i = nextCtr;
-            }
-        } else {
-            i = nextCtr;
-        }
-
+    for (long long i = 1; i < N; ++i) {
         cin >> curVal >> leftIdx >> rightIdx;
 
         nodes[i].val = curVal;
 
         if (leftIdx != -1) {
-            nodes[i].leftIdx = leftIdx;
             nodes[i].left = &nodes[leftIdx];
             nodes[i].left->breadth = nodes[i].breadth - 1;
             if (nodes[i].left->breadth < mostLeftBreadth) {
@@ -79,7 +60,6 @@ int main() {
         }
 
         if (rightIdx != -1) {
-            nodes[i].rightIdx = rightIdx;
             nodes[i].right = &nodes[rightIdx];
             nodes[i].right->breadth = nodes[i].breadth + 1;
             if (nodes[i].right->breadth > mostRightBreadth) {
@@ -88,17 +68,13 @@ int main() {
         } else {
             nodes[i].right = nullptr;
         }
-
-        current = &nodes[i];
     }
 
     mostLeftBreadth *= -1;
 
-    vector<int> columnSums(mostRightBreadth + mostLeftBreadth + 1, 0);
+    vector<long long> columnSums(mostRightBreadth + mostLeftBreadth + 1, 0);
 
-//    for (int i = 0; i < 1000000; ++i) {
     inorderSumUp(&nodes[0], columnSums);
-//    }
 
     for (auto columnSum : columnSums) {
         cout << columnSum << " ";
