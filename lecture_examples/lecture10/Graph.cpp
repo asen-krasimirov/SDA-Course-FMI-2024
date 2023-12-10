c7ea98dabf8a8afe1d3c4b7b95fc3a6fa9a6479b
asen-krasimirov <personalgrowth57@gmail.com>
2023-12-10 22:21:23 +0200

    adding implementation of Graph from lecture10

---
+++ lecture_examples/lecture10/Graph.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+using namespace std;
+
+class Graph {
+
+private:
+    int numVertices;
+    vector<int> *adjList;
+    bool directed;
+
+public:
+
+    Graph(int V, bool directed) : numVertices(V), adjList(new vector<int>[V]), directed(directed) {
+
+    }
+
+    void addEdge(int v, int w) {
+        if (!directed) {
+            adjList[w].push_back(v);
+        }
+        adjList[v].push_back(w);
+    }
+
+    vector<int> adj(int v) {
+        return adjList[v];
+    }
+
+    int vertices() {
+        return numVertices;
+    }
+
+};
