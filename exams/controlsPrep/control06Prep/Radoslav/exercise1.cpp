9219f137d439fb61aa192e4a7940d55c60abb64f
Radopeev <95441540+Radopeev@users.noreply.github.com>
2024-01-08 22:22:13 +0200

    add exam 6 prep

---
+++ exams/controlsPrep/control06Prep/Radoslav/exercise1.cpp
@@ -0,0 +1,43 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <unordered_set>
+#include <algorithm>
+using namespace std;
+
+void dfs(vector<vector<int>>& graph, int current, vector<bool>& visited) {
+    visited[current] = true;
+
+    for (auto& n : graph[current]) {
+        if (!visited[n]) {
+            dfs(graph, n, visited);
+        }
+    }
+}
+
+
+int main() {
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> graph(n + 1);
+        for (int j = 0; j < m; j++) {
+            int from, to;
+            cin >> from >> to;
+            graph[from].push_back(to);
+            graph[to].push_back(from);
+        }
+        int components = 0;
+        vector<bool> visited(n + 1);
+        for (int j = 0; j < n; j++) {
+            if (!visited[j]) {
+                dfs(graph, j, visited);
+                components++;
+            }
+        }
+        cout << components << " ";
+    }
+}
